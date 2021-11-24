6c32d88c65f9a49a831c0af31b1c970f57b24fcc
15bivolaku_m <15bivolaku_bexleygs.co.uk>
2021-11-24 13:24:49 +0000

    bubble sort

--- src/com/company/SeatingPlan.java
+++ src/com/company/SeatingPlan.java
@@ -11,14 +11,16 @@ public class SeatingPlan {
         //assign students desks
         //TODO Option to assign by alphabetically or by grade
         int temp;
-        if (students.size() > 1) {//check if the number of orders is larger than 1
-            for (int x=0; x < students.size(); x++){
-                for (int i=0; i < students.size()-x-1; i++){
-                    if (students.)
+        for (int i = 0; i < students.size(); i++){
+            for (int j = 0; j < students.size(); j++) {
+                if(students[i] > students[j + 1]){
+                    temp = students[j + 1];
+                    students[j + 1] = students[j];
+                    students[j] = temp;
                 }
             }
-
         }
+
 
         for (int i = 0; i< this.students.size(); i++){
             desks.setDeskName(this.students.getStudent(i).name , i);
