e18927d3f259c69451bd0764d6dfbe464055ab58
15bivolaku_m <15bivolaku_bexleygs.co.uk>
2021-11-24 09:28:51 +0000

    sort

--- src/com/company/SeatingPlan.java
+++ src/com/company/SeatingPlan.java
@@ -10,6 +10,15 @@ public class SeatingPlan {
         this.desks = new DeskLayout();
         //assign students desks
         //TODO Option to assign by alphabetically or by grade
+        int temp;
+        if (students.size() > 1) {//check if the number of orders is larger than 1
+            for (int x=0; x < students.size(); x++){
+                for (int i=0; i < students.size()-x-1; i++){
+                    if (students.)
+                }
+            }
+
+        }
 
         for (int i = 0; i< this.students.size(); i++){
             desks.setDeskName(this.students.getStudent(i).name , i);
