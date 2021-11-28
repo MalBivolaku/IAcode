a6bd0870fe0149ab1053808203a75e96e51dca1f
Mal Bivolaku <malbivolaku@gmail.com>
2021-11-28 21:29:11 +0000

    bubble sort

--- src/com/company/SeatingPlan.java
+++ src/com/company/SeatingPlan.java
@@ -1,29 +1,53 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class SeatingPlan {
     private TeachingClass students;
     private DeskLayout desks;
+    private int id;
 
-    public SeatingPlan(TeachingClass students , int boundary){
+    public SeatingPlan(TeachingClass students , int boundary , int id){
         students.setGradeBoundary(boundary);
         this.students = students.filterByGrade();
         this.desks = new DeskLayout();
+        this.id = id;
         //assign students desks
         //TODO Option to assign by alphabetically or by grade
-        int temp;
-        for (int i = 0; i < students.size(); i++){
-            for (int j = 0; j < students.size(); j++) {
-                if(students[i] > students[j + 1]){
-                    temp = students[j + 1];
-                    students[j + 1] = students[j];
-                    students[j] = temp;
-                }
-            }
-        }
+      int temp;
+      boolean sorted = false;
 
 
         for (int i = 0; i< this.students.size(); i++){
             desks.setDeskName(this.students.getStudent(i).name , i);
         }
     }
+    public TeachingClass getStudents(){
+        return students;
+    }
+
+    public int getId(){
+        return id;
+    }
+    public int compareTo(SeatingPlan seatingPlan){
+        int res = 0;
+        if (this.id < seatingPlan.getId()){
+            res = -1;
+        }
+        if (this.id > seatingPlan.getId()){
+            return res = 1;
+        }
+        return res;
+    }
+    public void bubbleSortStudents(){
+        boolean sorted = false;
+
+        while (!sorted){
+            sorted = true;
+            for (int i = 0; i < students.size(); i++){
+                if (students.get)
+            }
+        }
+    }
 }
+//https://stackabuse.com/bubble-sort-in-java/
